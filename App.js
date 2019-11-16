d0d76d81f4f663a58570ebeb34ebe9736bc0cdac
samgamage <sammygamage@gmail.com>
2019-11-16 01:24:24 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,257 @@
+import React, { Component } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+
+const { width, height } = Dimensions.get("window");
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.771707;
+const LONGITUDE = -122.4053769;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const GOOGLE_MAPS_APIKEY = "AIzaSyCG9aJFYbH7rQ1o0W-s23Q4von08JDkdmw";
+
+const MAPS_STYLE = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#f5f5f5"
+      }
+    ]
+  },
+  {
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#616161"
+      }
+    ]
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#f5f5f5"
+      }
+    ]
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#bdbdbd"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#eeeeee"
+      }
+    ]
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#757575"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#e5e5e5"
+      }
+    ]
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9e9e9e"
+      }
+    ]
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#ffffff"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#757575"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#dadada"
+      }
+    ]
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#616161"
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9e9e9e"
+      }
+    ]
+  },
+  {
+    featureType: "transit.line",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#e5e5e5"
+      }
+    ]
+  },
+  {
+    featureType: "transit.station",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#eeeeee"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#c9c9c9"
+      }
+    ]
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#9e9e9e"
+      }
+    ]
+  }
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // AirBnB's Office, and Apple Park
+    this.state = {
+      coordinates: [
+        {
+          latitude: 37.3317876,
+          longitude: -122.0054812
+        },
+        {
+          latitude: 37.771707,
+          longitude: -122.4053769
+        }
+      ]
+    };
+
+    this.mapView = null;
+  }
+
+  render() {
+    return (
+      <MapView
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: LATITUDE_DELTA,
+          longitudeDelta: LONGITUDE_DELTA
+        }}
+        provider={PROVIDER_GOOGLE}
+        style={StyleSheet.absoluteFill}
+        ref={c => (this.mapView = c)}
+        customMapStyle={MAPS_STYLE}
+      >
+        {this.state.coordinates.map((coordinate, index) => (
+          <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate} />
+        ))}
+        {this.state.coordinates.length >= 2 && (
+          <MapViewDirections
+            origin={this.state.coordinates[0]}
+            waypoints={
+              this.state.coordinates.length > 2
+                ? this.state.coordinates.slice(1, -1)
+                : null
+            }
+            destination={
+              this.state.coordinates[this.state.coordinates.length - 1]
+            }
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor="hotpink"
+            optimizeWaypoints={true}
+            onStart={params => {
+              console.log(
+                `Started routing between "${params.origin}" and "${params.destination}"`
+              );
+            }}
+            onReady={result => {
+              console.log("Distance: ${result.distance} km");
+              console.log("Duration: ${result.duration} min.");
+
+              this.mapView.fitToCoordinates(result.coordinates, {
+                edgePadding: {
+                  right: width / 20,
+                  bottom: height / 20,
+                  left: width / 20,
+                  top: height / 20
+                }
+              });
+            }}
+            onError={errorMessage => {
+              // console.log('GOT AN ERROR');
+            }}
+          />
+        )}
+      </MapView>
+    );
+  }
+}
+
+export default App;
